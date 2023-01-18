ed827ed55a053b6f6cad66eaf64e35c92d1e7c8a
wild8ear <wild8ear@yandex.ru>
2023-01-18 22:41:19 +0300

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -3,10 +3,7 @@ package ru.job4j;
 public class StudentInfo {
 
     public static void main(String[] args) {
-        int i = 1;
-        for(int count = 2; count<10; count++) {
-            int result = i * count;
-            System.out.println("" + i + " * " + count + " = " + result);
-        }
+        System.out.println("Pavel");
+        System.out.println("14.07.1991");
     }
 }
