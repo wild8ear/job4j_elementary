789d27f6da432bb17a8852fe09dd6f00678917b6
wild8ear <wild8ear@yandex.ru>
2023-01-18 22:26:25 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class Multiple {
+
+    public static void main(String[] args) {
+        int i = 1;
+        for(int count=2; count<10; count++) {
+            int result = i * count;
+            System.out.println("" + i + " * " + count + " = " + result);
+        }
+    }
+}
