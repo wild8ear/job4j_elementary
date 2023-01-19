48f30ef15411c0f7ce285c4955a10ad788d53f6a
wild8ear <wild8ear@yandex.ru>
2023-01-19 22:46:12 +0300

    4.2. Заголовок в файле java [#159036 #382635]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -3,7 +3,7 @@ package ru.job4j;
 public class Info {
 
     public static void main(String[] args) {
-        System.out.println("18.01.2023");
+        System.out.println("19.01.2023");
     }
 
 }
