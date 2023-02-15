29ab9d46380690caa8743a821aed87e04fca083d
wild8ear <wild8ear@yandex.ru>
2023-02-15 10:44:31 +0300

    1.2. Входные данные в тесте [#297750 #383617]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 public class X2Test {
 
     @Test
-    public void whenA1B1C1X1() {
+    public void whenA1B1C1X1Then3() {
         int a = 1;
         int b = 1;
         int c = 1;
@@ -19,7 +19,7 @@ public class X2Test {
     }
 
     @Test
-    public void whenA0B1C1X1() {
+    public void whenA0B1C1X1Then2() {
         int a = 0;
         int b = 1;
         int c = 1;
@@ -30,7 +30,7 @@ public class X2Test {
     }
 
     @Test
-    public void whenA1B1C0X1() {
+    public void whenA1B1C0X1Then2() {
         int a = 1;
         int b = 1;
         int c = 0;
@@ -41,7 +41,7 @@ public class X2Test {
     }
 
     @Test
-    public void whenA1B1C1X0() {
+    public void whenA1B1C1X0Then1() {
         int a = 1;
         int b = 1;
         int c = 1;
