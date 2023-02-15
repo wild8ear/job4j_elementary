3a0293069ddea2b94216cc1d3968ee7a8c13a94e
wild8ear <wild8ear@yandex.ru>
2023-02-15 16:46:28 +0300

    4.3.2. Сокращенный оператор || [#333784 #383770]

--- src/main/java/ru/job4j/condition/Club.java
+++ src/main/java/ru/job4j/condition/Club.java
@@ -3,6 +3,7 @@ package ru.job4j.condition;
 public class Club {
 
     public static void permission(boolean hasMoney, boolean beFriend) {
+
         if (hasMoney || beFriend) {
             System.out.println("I can go to the club.");
         } else {
