1a6d708a7ea40a49fd316d788543d7a23530d3be
wild8ear <wild8ear@yandex.ru>
2023-02-19 00:20:47 +0300

    5.4. Протеиновая диета (цикл while) [#156313 #393017]

--- src/test/java/ru/job4j/loop/FitnessTest.java
+++ src/test/java/ru/job4j/loop/FitnessTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FitnessTest {
+
     @Test
     public void whenIvanGreatNik() {
         int ivan = 95;
