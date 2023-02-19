6a73e6c861dfe8999dd10691e56d401b31bc26e6
wild8ear <wild8ear@yandex.ru>
2023-02-19 13:02:15 +0300

    5.2. Создать программу, вычисляющую факториал. [#193 #393009]

--- src/test/java/ru/job4j/loop/FactorialTest.java
+++ src/test/java/ru/job4j/loop/FactorialTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FactorialTest {
+
     @Test
     public void whenCalculateFactorialForFiveThenOneHundredTwenty() {
         int expected = 120;
