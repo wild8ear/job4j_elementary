56e40ddf9dd7ea3b1a715db09ee6554b65a63556
wild8ear <wild8ear@yandex.ru>
2023-02-20 23:23:22 +0300

    6.0.7. Массивы в тестах. [#226789]

--- src/main/java/ru/job4j/array/Square.java
+++ src/main/java/ru/job4j/array/Square.java
@@ -1,6 +1,7 @@
 package ru.job4j.array;
 
 public class Square {
+
     public static int[] calculate(int bound) {
         int[] rst = new int[bound];
         for (int index = 0; index < rst.length; index++) {
