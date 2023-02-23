9624a0cb044d8747e57b5cf24b6c806aa3cd1d6c
wild8ear <wild8ear@yandex.ru>
2023-02-23 14:16:30 +0300

    6.4.3. Слово заканчивается на ... [#156317 #394244]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -4,7 +4,7 @@ public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
         for (int index = 0; index < post.length; index++) {
-           if (word[word.length - post.length + index] != post[index]) {
+           if (word[post.length - 1 - index] != post[post.length - 1 - index]) {
                result = false;
                break;
            }
--- src/test/java/ru/job4j/array/EndsWithTest.java
+++ src/test/java/ru/job4j/array/EndsWithTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EndsWithTest {
+
     @Test
     public void whenEndWithPrefixThenTrue() {
         char[] word = {'H', 'e', 'l', 'l', 'o'};
