5bcf97fe8f4fbcb09709e8bfcd2694237b4d0d69
wild8ear <wild8ear@yandex.ru>
2023-02-27 22:43:00 +0300

    6.9. String - это массив [#504884]

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,37 @@
+package ru.job4j.array;
+
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        if (name.isEmpty()) {
+            return false;
+        }
+        char[] array = name.toCharArray();
+        if (Character.isUpperCase(array[0])) {
+            return false;
+        }
+        if (!isLowerLatinLetter(name.codePointAt(0))) {
+            return false;
+        }
+
+        for (int index = 1; index < array.length; index++) {
+            int code = Character.codePointAt(array, index);
+            if (!(isLowerLatinLetter(code) || isUpperLatinLetter(code) || isSpecialSymbol(code) || Character.isDigit(array[index]))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+        return code == 36 || code == 95;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return code > 64 && code < 91;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return code > 96 && code < 123;
+    }
+}
