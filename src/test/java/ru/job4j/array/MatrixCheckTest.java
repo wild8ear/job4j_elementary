7b7a5fa0af499e42aa976c340efc72a73ecb821d
wild8ear <wild8ear@yandex.ru>
2023-02-28 08:06:38 +0300

    6.7.4. Выигрышные комбинации в сокобан [#53859 #395416]

--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MatrixCheckTest {
-
     @Test
     public void whenHasMonoHorizontal() {
         char[][] input = {
